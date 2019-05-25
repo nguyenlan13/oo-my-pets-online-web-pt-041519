403141e36996ba06eb8fab11a4f9248fba48f883
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-25 22:48:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,45 @@
+require "pry"
+
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
+    
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+  
+  def walk_dogs
+     @pets[:dogs].each do |dog|
+       dog.mood = "happy"
+     end
+  end
+  
+  def play_with_cats
+     @pets[:cats].each do |cat|
+       cat.mood = "happy"
+     end
+  end
+  
+  def feed_fish
+     @pets[:fishes].each do |fish|
+       fish.mood = "happy" 
+     end
+  end
 end
